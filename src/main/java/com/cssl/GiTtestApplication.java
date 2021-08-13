8f6f746c08d44511e55a8eed6868a03d0c9eac5d
shesir <2247114972@qq.com>
2021-08-13 17:30:43 +0800

    V1.0

---
+++ src/main/java/com/cssl/GiTtestApplication.java
@@ -0,0 +1,13 @@
+package com.cssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiTtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiTtestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cssl/pojo/Users.java
@@ -0,0 +1,12 @@
+package com.cssl.pojo;
+
+public class Users {
+    private String ename="123";
+    private Integer sal;
+
+    public Integer getSal() {
+        return sal;
+    }
+    private int empno=3;
+    private int deptno;
+}
